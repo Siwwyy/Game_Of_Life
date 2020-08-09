90cac40d3f2bde9acb0d14750f5cf4b57b6a5a64
Damian Andrysiak <andrydamian@gmail.com>
2020-08-09 21:39:23 +0200

    up

--- Game_Of_Life/Game_Of_Life_ver2/main.cpp
+++ Game_Of_Life/Game_Of_Life_ver2/main.cpp
@@ -1,17 +1,11 @@
-#include <iostream>
 #include <windows.h>
-#include <string>
-#include <cstdlib>
-#include <cstdint>
 
 
 #include "Animal_Manager.hpp"
-#include "Animal.hpp"
 
 int main(int argc, char* argv[])
 {
 	using Creature::Animal_Manager;
-	using Creature::Animal;
 
 	Animal_Manager Obj;
 	Obj.Run_Simulation();
